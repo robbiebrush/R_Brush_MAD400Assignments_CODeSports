ecc737a3bf5b3e066a827ecaac37b556e4e8a200
Robbie Brush <w0741993@myscc.ca>
2022-04-18 13:58:34 -0400

    Readded log service code, push didn't work last night

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Content } from './helper-files/content-interface';
 import { CODInfoService } from './Services/codinfo.service';
+import {LogUpdateService} from "./log-update.service";
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ export class AppComponent implements OnInit {
   title = 'learning-angular';
   @Input() spContentList: Content[];
 
-  constructor(private codInfoService: CODInfoService) {
+  constructor(private codInfoService: CODInfoService, private logService: LogUpdateService) {
     this.spContentList = [];
    }
 
-  
+
   ngOnInit(): void {
+    this.logService.init();
   }
 }
--- src/app/log-update.service.ts
+++ src/app/log-update.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogUpdateService {
+  private snackBar: MatSnackBar | undefined;
 
-  constructor() { }
+  constructor(private updates: SwUpdate) { }
+
+  public init(){
+    this.updates.versionUpdates.subscribe(event => {
+      switch (event.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version:
+          ${event.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          // @ts-ignore
+          let snackBarRef = this.snackBar.open('Update is ready', 'Update');
+
+          snackBarRef.afterDismissed().subscribe(() => {
+            console.log('The snackbar was dismissed');
+          });
+
+          snackBarRef.onAction().subscribe(() => {
+            console.log('Updating!');
+            this.updates.activateUpdate().then(() =>
+              document.location.reload());
+          });
+          break;
+      } });
+  }
 }
